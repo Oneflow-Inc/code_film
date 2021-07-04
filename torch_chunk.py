8bf18a7895a0d2de4cac43e987b2739f9609e622
Yao Chi <later@usopp.net>
2021-07-04 09:36:03 +0800

    refine chunk tutorial
    refine

--- torch_chunk.py
+++ torch_chunk.py
@@ -1,4 +1,5 @@
-# Pytorch 教学：torch.chunk
+# Pytorch 算子介绍：torch.chunk
+# https://pytorch.org/docs/stable/generated/torch.chunk.html
 
 #导入必要的库
 import torch
@@ -12,17 +13,27 @@ x
 
 # chunk 格式为：torch.chunk(输入, 被切分后矩阵数量, 切分维度(dim = 维度))
 
+res_list = torch.chunk(x, 5, dim=0)
+[ chunk.shape for chunk in res_list] # 显示切分后的各个小张量的形状
+res_list # 显示切分后的内容
+
+# 其中，当dim = 0，chunk就会把矩阵“按行横着切”；当dim = 1时，chunk就会把矩阵“按列竖着切”
+# 针对以上5行3列的矩阵。
+
+# 以下就是按行切：
 torch.chunk(x, 5, dim=0)
 
-# 其中，当dim = 0，chunk就会把矩阵“横着切”；当dim = 1时，chunk就会把矩阵“竖着切”
-# 最开始我们已经随机取了一个5行3列的矩阵。若我们想要将矩阵以行分开的话,上面这行代码(dim=0)即可做到
+# 以下就是按列切：
+torch.chunk(x, 5, dim=1)
 
-# 当然，若想以列分开：
+# 当然，其实 dim 可以是任意维度
+x = torch.rand(5, 3, 4, 8)
+res_list = torch.chunk(x, 4, dim=3)
+[ chunk.shape for chunk in res_list] # 显示切分后的各个小张量的形状
 
-torch.chunk(x, 3, dim=1)
 
-# 值得注意的是，chunk函数中的第二个输入(被切分后矩阵数量)不一定需要被矩阵形状所整除
-
-torch.chunk(x, 3, dim=0)
-
-# 我们可以看到，chunk会尽可能均分输入矩阵，但最后一个矩阵的大小为 矩阵形状除以被切分矩阵数量 的余数
+# 值得注意的是，chunk 方法的第二个参数(指定切分的数量)不一定需要被维度大小所整除
+# 当不能整除时，最后一个张量的特定维度大小为余数
+x = torch.rand(7, 3)
+res_list = torch.chunk(x, 3, dim=0)
+[ chunk.shape for chunk in res_list] # 显示切分后的各个小张量的形状
